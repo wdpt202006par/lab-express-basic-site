915a9b903ab66de050a3dd1a1f59047e752c7a83
toufiq23 <drrameraaron023@gmail.com>
2020-08-08 23:55:44 +0200

    All Done

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const app = express();
 app.use(express.static('public'))
 
 app.get('/home', function(request, response, next) {
-
-    response.sendFile(__dirname + '/views/home.html');
+	response.sendFile(__dirname + '/views/home.html');
 })
 
 app.get('/about', function(request, response, next) {
-
-    response.sendFile(__dirname + '/views/about.html');
+	response.sendFile(__dirname + '/views/about.html');
 })
 
 app.get('/works', function(request, response, next) {
-
-    response.sendFile(__dirname + '/views/works.html');
+	response.sendFile(__dirname + '/views/works.html');
 })
 
 app.get('/photos', function(request, response, next) {
-
-    response.sendFile(__dirname + '/views/photos.html');
+	response.sendFile(__dirname + '/views/photos.html');
 })
 
-app.listen(3001);
+app.listen(3000);
