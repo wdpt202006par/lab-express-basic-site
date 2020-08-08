25e9c4a7fedb0855178b2148d470105f0bfa9580
Sarah Damag <sarahdamag@gmail.com>
2020-08-08 14:32:54 +0200

    lab Beyonce fini

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express')
+const express = require("express");
 
+const app = express();
+app.use(express.static("public"));
 
-const app = express()
- app.use(express.static('public'))
+app.get("/home", function (request, response, next) {
+  response.sendFile(__dirname + "/views/home.html");
+});
 
- app.get('/home', function(request, response, next){
-    response.sendFile(__dirname+'/views/home.html')
- })
+app.get("/about", function (request, response, next) {
+  response.sendFile(__dirname + "/views/about.html");
+  // response.send('<h1>coucou</h1>');
+  console.log("you are in the aboout page");
+});
 
- app.get('/about', function(request, response, next){
-    response.sendFile(__dirname+'/views/about.html')
-    // response.send('<h1>coucou</h1>');
-    console.log('you are in the aboout page')
- })
+app.get("/works", function (request, response, next) {
+  response.sendFile(__dirname + "/views/works.html");
+});
 
- app.get('/works', function(request, response, next){
-    response.sendFile(__dirname+'/views/works.html')
- })
+app.get("/gallery", function (request, response, next) {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
 
- app.listen(3000)
+app.listen(3000);
