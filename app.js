e3f1b3d16cb1254ccd9980f0937c4b30c4c6eea8
kardelen-karatas <kardelen.karatas@gmail.com>
2020-08-08 14:31:56 +0200

    ajout des pages home et works

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/home', (request, response) => {
-    console.log(request);
-    response.send('<h1>Welcome Ironhacker. :)</h1>');
-  });
+app.get('/home', (request, response) => response.sendFile(__dirname + '/views/home.html'));
 
 app.get('/about', (request, response) => response.sendFile(__dirname + '/views/about.html'));
 
-app.listen(3005, () => console.log('My first app listening on port 3005! '));
+app.get('/works', (request, response) => response.sendFile(__dirname + '/views/works.html'));
+
 
