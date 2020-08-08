a7d310318552f7f13b2c14569f4bfd8377473c10
Claire Lacanal <lacanal.claire@gmail.com>
2020-08-08 12:59:14 +0200

    elon Musk projet v1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'))
+
+app.get('/home', function(request, response, next) {
+
+    response.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/about', function(request, response, next) {
+
+    response.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/works', function(request, response, next) {
+
+    response.sendFile(__dirname + '/views/works.html');
+})
+
+app.get('/photos', function(request, response, next) {
+
+    response.sendFile(__dirname + '/views/photos.html');
+})
+
+app.listen(3000);
