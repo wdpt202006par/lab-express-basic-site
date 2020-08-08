b9ec0973e5b5349a30af660670960511ae2fd15a
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-08-08 12:50:17 +0200

    1st commit sans pimp

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/home', function (request, response, next) {
+    response.sendFile(__dirname+"/views/home.html");
+
+})
+
+app.get('/about', function (request, response, next) {
+    response.sendFile(__dirname+"/views/about.html");
+
+})
+
+app.get('/work', function (request, response, next) {
+    response.sendFile(__dirname+"/views/work.html");
+
+})
+
+app.listen(3000);
